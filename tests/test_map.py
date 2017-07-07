45a4e442c86e045f35f6c50394e9f5141fe1af00
Innocent Asiimwe <asiimwe@outlook.com>
2017-07-07 12:15:50 +0300

    Import the LearningMap class

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,4 +1,5 @@
 import unittest
+from classes import LearningMap
  
 
 class TestMap(unittest.TestCase):
@@ -13,11 +14,11 @@ class TestMap(unittest.TestCase):
         list_before = len(self.map.skills_list)
         self.map.add_skill(self.skill)
         list_after = len(self.map.skills_list)
-        self.assertEqual([list_before, list_after], [0, 1] msg = "skill should be added to the skills_list")
+        self.assertEqual([list_before, list_after], [0, 1], msg = "skill should be added to the skills_list")
 
     def test_skill_is_skill_object(self):
         """"""
-        self.assertRaises(TypeError, self.map.add_skill, 'skill' msg = "Input should be a skill object")
+        self.assertRaises(TypeError, self.map.add_skill, 'skill', msg = "Input should be a skill object")
 
 if __name__ = "__main__":
     unittest.main()
